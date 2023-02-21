f848f79c5721f338b0c7d6bafd03ec1a27dbd8d1
AlbanTeng <jialeteng1984@163.com>
2023-02-21 22:09:14 +0800

    updated 21 Feb

--- Back-End/app.py
+++ Back-End/app.py
@@ -8,19 +8,20 @@ CORS(app , resources={r"/*": {"origins": "*"}})
 #MySQL DB connection configuration;
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'root'
-app.config['MYSQL_DB'] = 'db'
+app.config['MYSQL_PASSWORD'] = '123456'
+app.config['MYSQL_PORT'] = 13306
+app.config['MYSQL_DB'] = 'user_info'
+
 mysql = MySQL(app)
 
-@app.route('/jobs')
-def jobs():
+
+@app.route('/users')
+def users():
     cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM jobs")
+    cur.execute("SELECT * FROM users")
     fetchdata = cur.fetchall()
     cur.close()
     return jsonify(fetchdata)
-    
- 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+        app.run(debug=True)
